6a50b49af8cf9bd93ede758c3470aa216a1b5bf6
Robin <24046@ma-web.nl>
2022-09-21 15:14:59 +0200

    Opdracht Hello World/ Hello You voor leren React, Mijne is genaamd HelloMe

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//Importing React
+import React from "react";
+
+
+//importing ReactDOM
+import reactDom from "react-dom";
+
+//Loading the app
+import App from "./App";
+
+//adding it to the dom
+reactDom.render(<App />, document.getElementById("root"));
